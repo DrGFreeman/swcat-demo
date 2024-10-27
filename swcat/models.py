0fdc6587417a16c5f245784e788f7e7ea603f307
DrGFreeman <DrGFreeman@users.noreply.github.com>
2024-10-27 14:43:16 -0400

    Add software description and code URL

--- swcat/models.py
+++ swcat/models.py
@@ -3,10 +3,13 @@ from datetime import date
 from pydantic import BaseModel
 from pydantic import EmailStr
 from pydantic import Field
+from pydantic import HttpUrl
 from pydantic import UUID4
 
 
 class SoftwareBase(BaseModel):
+    code_url: HttpUrl | None = Field(default=None)
+    description: str | None = Field(default=None, max_length=280)
     display_name: str = Field(min_length=2)
     maintainer_name: str | None = Field(default=None)
     maintainer_email: EmailStr
